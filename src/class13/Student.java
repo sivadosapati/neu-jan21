0cca69152526dcf255229867479fa0db16292be8
sivad <sivad@localhost>
2021-03-11 15:44:38 -0800

    Commit code from class13

--- src/class13/Student.java
+++ src/class13/Student.java
@@ -12,6 +12,40 @@ public class Student {
 	
 
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((birthPlace == null) ? 0 : birthPlace.hashCode());
+		result = prime * result + Float.floatToIntBits(gpa);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (birthPlace == null) {
+			if (other.birthPlace != null)
+				return false;
+		} else if (!birthPlace.equals(other.birthPlace))
+			return false;
+		if (Float.floatToIntBits(gpa) != Float.floatToIntBits(other.gpa))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 	public Student(String name, String birthPlace, float gpa) {
 		super();
 		this.name = name;
